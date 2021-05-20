da69c4548919aa02d17a2d857374ec72f43bbf67
ahammed3216 <59572889+ahammed3216@users.noreply.github.com>
2021-05-20 11:17:05 +0530

    Delete student directory

--- student/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import home,LoginView,register
-
-urlpatterns=[
-    path('',home,name='home-page'),
-    path('login/',LoginView,name='login-page'),
-    path('register/',register,name='register-page')
-]
